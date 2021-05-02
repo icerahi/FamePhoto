f222a6a085056f88b23691c059a810f49cff3667
rahi <zanjarwhite@gmail.com>
2021-05-03 00:06:11 +0600

    added custom jwt auth response

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token , refresh_jwt_token
 urlpatterns = [
     path('login/',obtain_jwt_token),#default jwt login
+    path('token_refresh/',refresh_jwt_token),
 ]
