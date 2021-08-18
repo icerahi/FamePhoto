b7e534cd16064735f08582d271e04a1b026af18c
icerahi <zanjarwhite@gmail.com>
2021-08-18 11:04:22 +0600

    comment index view

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('',RedirectView.as_view(url='api/photos/'), name='go-to-django'),
     path('api/photos/',include(('apps.photos.urls','photos'),namespace='photos')),
     path('api/accounts/',include(('apps.accounts.urls','accounts'),namespace='accounts')),
     path('api/albums/',include(('apps.albums.urls','albums'),namespace='albums')),
@@ -19,11 +20,11 @@ if settings.DEBUG:
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-    urlpatterns += [
-        re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-    ]
+#     urlpatterns += [
+#         re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+#     ]
 
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-    ]
+# if not settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+#     ]
